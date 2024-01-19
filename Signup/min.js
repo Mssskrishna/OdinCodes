52b5a9d0c0f56677a8da4bfead0f42ea677d37ab
MSSSKrishna <mssskrishna0734@gmail.com>
2024-01-19 16:44:00 +0530

    patterns, box-shadow, added

--- Signup/min.js
+++ Signup/min.js
@@ -1,11 +1,18 @@
+// let password = document.querySelector("#password")
+// let passChange = document.querySelector("#password")
+let confirm = document.querySelector("#confirm")
+let conchange = document.querySelector("#confirm")
+
+confirm.addEventListener('input',validatepassword);
+
 function validatepassword() {
-    let password = document.querySelector('#password');
-    let confirm = document.querySelector('#confirm')
+    let password = document.querySelector('#password').value;
+    let confirm = document.querySelector('#confirm').value;
     let input = document.querySelector("input[type='password']")
     console.log(password.textContent)
-    if (password.textContent === confirm.textContent) {
-        input.style.cssText = "border:2px solid green"
+    if (password === confirm) {
+        conchange.style.cssText = "border:2px solid green"
     } else {
-        input.style.cssText = "border:2px solid red"
+        conchange.style.cssText = "border:2px solid red"
     }
 }
