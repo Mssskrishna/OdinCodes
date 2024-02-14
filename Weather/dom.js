8d4e186dfdc3794c7a0f73e7d57f4df868870b1a
MSSSKrishna <mssskrishna0734@gmail.com>
2024-02-14 19:26:22 +0530

    style and js is added

--- Weather/dom.js
+++ Weather/dom.js
@@ -1,32 +1,46 @@
-import { getWeatherData, getWeathertemp } from "./weatherutil.js";
+import { getWeathertemp } from "./weatherutil.js";
 
-const button = document.querySelector('.button');
-const temperaturetoo = document.querySelector('#temperature')
-let initial = "london"
-let temperatureUnit = 'C'
-async function renderdata() {
-    const cityname = document.querySelector('#cityname').value
-    let temperature, feelslike, data;
-    if (!cityname) {
-        data = await getWeathertemp(initial, temperatureUnit);
+const button = document.querySelector('.search');
+const temperatureToo = document.querySelector('#temperature');
+const inputContent = document.querySelector('#cityname');
+const updateTemp = document.querySelector('.temperature');
+const updateFeel = document.querySelector('.feelslike');
+const imageTemp = document.querySelector('.image');
+const type = document.querySelector('.input');
+const rain = document.querySelector('.rain');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const cityi = document.querySelector('.city');
 
-    } else {
-        data = await getWeathertemp(cityname, temperatureUnit);
-        initial = cityname;
-    }
-    temperature = data[0];
-    feelslike = data[1];
-    console.log("temperature: " + temperature);
-    console.log("feelslike : " + feelslike)
+let initial = "london";
+let temperatureUnit = 'C';
+
+async function renderData(city = initial) {
+    const data = await getWeathertemp(city, temperatureUnit);
+    initial = city;
+    updatePage(data);
+}
+
+function updatePage(data) {
+    updateTemp.innerHTML = `${data.temperature}°${temperatureUnit}`;
+    updateFeel.innerHTML = `${data.feelslike}°${temperatureUnit}`;
+    imageTemp.src = data.imageurl;
+    type.innerHTML = data.type;
+    rain.innerHTML = `${data.rain}%`;
+    humidity.innerHTML = `${data.humidity}%`;
+    wind.innerHTML = `${data.wind}kmph`;
+    cityi.innerHTML = data.city;
 }
 
 button.addEventListener('click', async () => {
-    renderdata()
-
+    await renderData(inputContent.value);
+    inputContent.value = '';
 });
-temperaturetoo.addEventListener('click', () => {
-    temperatureUnit = temperatureUnit === 'F' ? 'C' : 'F';
-    temperaturetoo.textContent = temperatureUnit;
-    renderdata()
-})
 
+temperatureToo.addEventListener('click', async () => {
+    temperatureUnit = temperatureUnit === 'F' ? 'C' : 'F';
+    temperatureToo.textContent = temperatureUnit === 'F' ? 'Fahrenheit' : 'Celsius';
+    renderData();
+});
+
+renderData();
--- Weather/weatherutil.js
+++ Weather/weatherutil.js
@@ -1,15 +1,21 @@
-const weather_api = "4d70bc4d62504a0da68143152241302"
+const weather_api = "66af24c06e474bff94a45053241402"
 async function getWeather(city) {
-    const weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=${weather_api}&q=${city}`)
+    const weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=${weather_api}&q=${city}`,{mode:"cors"})
     const weatherj = await weather.json()
     return weatherj;
 }
 
-async function getWeathertemp(city, mode) {
+async function getWeathertemp(city, unit) {
     const weather = await getWeather(city)
     const temperature = unit === 'F' ? weather.current.temp_f : weather.current.temp_c;
-    const feelsLike = unit === 'F' ? weather.current.feelslike_f : weather.current.feelslike_c;
-    return [temperature, feelsLike];
+    const feelslike = unit === 'F' ? weather.current.feelslike_f : weather.current.feelslike_c;
+
+    const imageurl = 'img/'+weather.current.condition.icon.slice(21)
+    const humidity = weather.current.humidity;
+    const wind = weather.current.wind_kph;
+    const rain = weather.current.precip_mm;
+    const type = weather.current.condition.text;
+    return {temperature, feelslike,imageurl,humidity,wind,rain,city,type};
 }
 
-export { getWeathertemp, getWeatherData }
+export { getWeathertemp }
