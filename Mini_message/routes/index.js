3d8e25f02c070691e1536b41ea0a626d24c776cf
MSSSKrishna <mssskrishna0734@gmail.com>
2024-08-12 17:43:12 +0530

    routes are updated:using db

--- Mini_message/routes/index.js
+++ Mini_message/routes/index.js
@@ -1,33 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../db/query');
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date()
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date()
+//   }
+// ];
+
+router.get('/', async function (req, res, next) {
+  try {
+    const messages = await db.getAllMessages();
+
+    res.render('index', { title: 'Message Board', messages });
+  } catch (error) {
+    next(error); 
   }
-];
-
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 router.get('/new', function (req, res, next) {
   res.render('form');
 });
 
-router.post('/new', function (req, res, next) {
-  const messageText = req.body.message;
-  const messageUser = req.body.username;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
-  res.redirect('/');
-});
+async function insertMessage(req, res) {
+  const messageText = req.body.message.trim();
+  const messageUser = req.body.username.trim();
+  try {
+    await db.insertMessage(messageText, messageUser);
+    res.redirect('/')
+  }
+  catch (err) {
+    res.status(500).send("An error occured during inserting message");
+  }
+
+}
+
+router.post('/new', insertMessage);
 
 module.exports = router;
