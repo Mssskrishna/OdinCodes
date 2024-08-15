cde3f8a4cd6d4d07a208acac7d23343e4ea567e6
MSSSKrishna <mssskrishna0734@gmail.com>
2024-08-15 14:35:15 +0530

    TODO: Insert and Delete queries

--- Inventory/db/query.js
+++ Inventory/db/query.js
@@ -5,22 +5,25 @@
 
 const pool = require('./pool');
 
-// const db = new 
-async function getAllFirearms(category = null) {
+
+
+async function getAllFirearms(category = null,limit,offset) {
     try {
         const query = category
-            ? 'SELECT * FROM firearms WHERE category = $1;'
-            : 'SELECT * FROM firearms;';
-        const params = category ? [category] : [];
+            ? 'SELECT * FROM firearms WHERE category = $1 LIMIT $2 OFFSET $3;'
+            : 'SELECT * FROM firearms LIMIT $1 OFFSET $2;';
+        const params = category ? [category, limit, offset] : [limit, offset];
 
         const { rows } = await pool.query(query, params);
         // console.log(rows);
-        return rows;
+
+        return { rows };
     } catch (error) {
         console.error('Error fetching firearms:', error);
-    } 
+    }
+    // limit += 10
 }
-
+// getAllFirearms()
 async function getAllCategories() {
     try {
         const query = 'SELECT DISTINCT category FROM firearms';
@@ -31,6 +34,8 @@ async function getAllCategories() {
         console.error("Error fetching categories", error);
     }
 }
+
+
 module.exports = {
     getAllFirearms,
     getAllCategories
