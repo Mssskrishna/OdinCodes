cd6c79c09c2555b5ff53602ddae4dbd5a5a0c4d2
MSSSKrishna <mssskrishna0734@gmail.com>
2024-08-15 14:36:48 +0530

    routers,views added

--- Inventory/routes/index.js
+++ Inventory/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const catrouter = require("../controllers/firearms");
-
+const catrouter = require("../controllers/categories");
+const firerouter = require('../controllers/firearms')
 /* GET home page. */
-router.get('/', catrouter.getAllCategories);
+// router.get('/', catrouter.getAllCategories);
 
+router.get('/category',catrouter.getCategories);
+router.get('/firearms',firerouter.getFirearms)
+router.get('/firearm',firerouter.getFirearmsLmit)
+// router.get('/newCategory',catrouter.updateCategory);
 
 module.exports = router;
